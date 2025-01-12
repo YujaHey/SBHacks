0e258a30ee252c2294a74c31a27b2d19ec30c335
ujahey <119145741+YujaHey@users.noreply.github.com>
2025-01-12 08:49:21 -0800

    Update bird_detection.py

--- bird_detection.py
+++ bird_detection.py
@@ -1,48 +1,49 @@
-import cv2
-import datetime
+import cv2 as cv
+# import datetime
 import os
 from ultralytics import YOLO
 
-# Loading pretrained YOLO model (will be downloaded on first run)
-model = YOLO("model/yolov8n.pt", "v8")
+# loops over the bird image files
+def path_list(path):
+    directory = {'path': path,'categories': 0,'images': 0}
+    file_path = []
+    for dirpath,dirnames,filenames in os.walk(directory['path']):
+        for filename in filenames:
+            # Get the full file path
+            path = os.path.join(dirpath, filename)
+            file_path.append(path)
+        directory['categories'] += len(dirnames)
+        directory['images'] += len(filenames)
+    print(f'there is {directory['categories']} subfolder classes and {directory['images']} images in total')
+    return file_path
 
-# Set dimensions of video frames
-frame_width = 1280
-frame_height = 720
-
-directory = {'path': r'/Users/Mints/Documents/GitHub/SBHacks/Bird Speciees Dataset','categories': 0,'images': 0}
-for dirpath,dirnames,filenames in os.walk(directory['path']):
-    directory['categories'] += len(dirnames)
-    directory['images'] += len(filenames)
+# i = 0
+# while i < directory['images']-1:
+#     img = cv.imread(file_path[i],-1)
+#     cv.resize(img,(frame_width,frame_height))
+#     cv.imshow('display', img)
+#     i += 1
+#     if cv.waitKey(30) == ord('q'):
+#         break
 
 # Only save an image on frame 0
-frame_count = 0
+def img(path,text):
+    # Loading pretrained YOLO model (will be downloaded on first run)
+    model = YOLO("model/yolov8n.pt", "v8")
 
-if not cap.isOpened():
-    print("Cannot open video stream")
-    exit()
-
-while True:
-    ret, frame = cap.read()
-
-    if not ret:
-        print("No video frame available")
-        break
-    
-    # Resize the frame
-    frame = cv2.resize(frame, (frame_width, frame_height))
+    img = cv.imread(path,-1)
 
     # Do prediction on image, with confidence greater than 80%
-    detect_params = model.predict(source=[frame], conf=0.8, save=False)
+    detect_params = model.predict(source=[img], conf=0.5, save=False)
 
     DP = detect_params[0].numpy()
+    font = cv.FONT_HERSHEY_SIMPLEX
 
     if len(DP) != 0:
         for i in range(len(detect_params[0])):
-
             boxes = detect_params[0].boxes
             box = boxes[i]
-            clsID = box.cls.numpy()[0]
+            # clsID = box.cls.numpy()[0]
             conf = box.conf.numpy()[0]
             bb = box.xyxy.numpy()[0]
             c = box.cls
@@ -52,42 +53,38 @@ while True:
         # If the class name contains the word 'bird', do something with the frame
         if 'bird' in class_name.lower():
 
-            if frame_count == 0:
-                current_time = datetime.datetime.now()
-                filename = os.path.join("images", current_time.strftime("bird_%Y-%m-%d_%H-%M-%S-%f.jpg"))
-                success = cv2.imwrite(filename, frame)
-
-            if frame_count == 10:
-                frame_count = 0
-            else:
-                frame_count += 1
-
             # Draw green rectangle around the object
-            cv2.rectangle(
-                frame,
+            cv.rectangle(
+                img,
                 (int(bb[0]), int(bb[1])),
                 (int(bb[2]), int(bb[3])),
                 (0, 255, 0),
                 3,
             )
             # Add some text labelling to the rectangle
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(
-                frame,
-                class_name + " " + str(round(conf, 3)) + "%",
+            cv.putText(
+                img,
+                str(text),
                 (int(bb[0]), int(bb[1]) - 10),
                 font,
                 1,
-                (255,255,255),
+                (0, 0, 255),
                 2,
+                lineType=cv.LINE_AA
+            )
+    else:
+        cv.putText(
+                img,
+                str(text),
+                (100,100),
+                font,
+                1.5,
+                (0, 0, 255),
+                3,
+                lineType=cv.LINE_AA
             )
 
     # Display the frame onscreen
-    cv2.imshow("Object Detection", frame)
-
-    # End program when q is pressed
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+    cv.imshow("Object Detection", img)
+    cv.waitKey(5000)
+    cv.destroyAllWindows()
